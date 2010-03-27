c85b1b111d46004f922f58ad67b45ce90ff32218
argl <andi@watwet.com>
2010-03-27 01:09:22 +0100

    element form error ui stuff, form builder refinements, tab-to-space conversion of some files

--- lib/humpyard/action_view/form_helper.rb
+++ lib/humpyard/action_view/form_helper.rb
@@ -5,6 +5,15 @@ module Humpyard
         inner_haml = capture_haml(Humpyard::FormBuilder.new(self, record, options), &block)
         render :partial => '/humpyard/forms/form', :locals => {:options => options, :inner_haml => inner_haml}
       end
+      
+      # execute a block with a given locale
+      def with_locale(locale, &block)
+        old_locale = I18n.locale
+        I18n.locale = locale
+        yield(locale)
+        I18n.locale = old_locale
+      end
+      
     end
   end
 end
