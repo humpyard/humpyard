ea0759e54d44e1a7b882a2d7333e16217b402bee
argl <andi@watwet.com>
2010-04-02 16:26:23 +0200

    fixed test typo, fixed drawing order on element edit adornments

--- spec/models/humpyard/element_spec.rb
+++ spec/models/humpyard/element_spec.rb
@@ -25,7 +25,7 @@ describe Humpyard::Element do
     e.valid?.should eql true
   end
   
-  it "is invalid if display_until is after display_from" do
+  it "is valid if display_until is after display_from" do
     e = Factory.build(:element)
     e.display_from = Time.zone.now - 1.week
     e.display_until = Time.zone.now + 1.week
