7bec557250be6beff63f278d74859957e7485d1c
Sven G. Brönstrup <broenstrup@spom.net>
2010-04-08 11:54:13 +0200

    routes fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do |map|
   # Map admin controllers
+  
+  # Workaround as the create route does not seem to work with latest rails
+  match "/#{Humpyard::config.admin_prefix}/element/create(.:format)" => 'humpyard/elements#create'
+  match "/#{Humpyard::config.admin_prefix}/page/create(.:format)" => 'humpyard/pages#create'
+  
   scope "/#{Humpyard::config.admin_prefix}" do 
-    resources :humpyard_pages, :controller => 'humpyard/pages', :path => :pages, :only => [:index, :new, :edit, :update, :show, :destroy] do
+    resources :humpyard_pages, :controller => 'humpyard/pages', :path => :pages, :only => [:index, :new, :create, :edit, :update, :show, :destroy] do
       member do
         post :move
       end
     end
-    resources :humpyard_elements, :controller => 'humpyard/elements', :path => :elements, :only => [:new, :edit, :update, :show, :destroy] do
+    resources :humpyard_elements, :controller => 'humpyard/elements', :path => :elements, :only => [:new, :create, :edit, :update, :show, :destroy] do
       member do
         get :inline_edit
         post :move
       end
     end
   end
-  
-  # Workaround as the create route does not seem to work with latest rails
-  match "/#{Humpyard::config.admin_prefix}/element/create(.:format)" => 'humpyard/elements#create'
-  match "/#{Humpyard::config.admin_prefix}/page/create(.:format)" => 'humpyard/pages#create'
   
   # Map "/" URL
   root :to => 'humpyard/pages#show'
