08bccb8bf94700e6f6c7d642e53d86df063b5ccc
Sven G. Brönstrup <broenstrup@spom.net>
2010-09-22 21:11:49 +0200

    improved RedCloth support

--- app/models/humpyard/elements/text_element.rb
+++ app/models/humpyard/elements/text_element.rb
@@ -5,12 +5,37 @@ module Humpyard
     class TextElement < ::ActiveRecord::Base
       acts_as_humpyard_element :system_element => true
       
-      attr_accessible :content
+      attr_accessible :content, :html_content
       
       require 'globalize'
       
       translates :content
       validates_presence_of :content
+      
+      def html_content
+        if content.blank?
+          ''
+        else
+          if Object.const_defined?('RedCloth')
+            RedCloth.new(content).to_html.html_safe
+          else
+            content.gsub("\n", "<br />").html_safe
+          end
+        end
+      end
+
+      def html_content= content
+        if content.blank?
+          ''
+        else
+          if Object.const_defined?('RedCloth')
+            require 'html_to_textile'           
+            self.content = HtmlToTextile.new(content.gsub("\n", ' ')).to_textile
+          else
+            self.content = content.gsub(/<br[^>]*>/, "\n").gsub(/<[^>]*>/, '')
+          end
+        end
+      end
     end
   end
 end
